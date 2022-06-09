6dab63e7c877c4c45bfd4841b6e6752525a7e4ce
GitHub Action <action@github.com>
2022-06-09 14:22:01 +0000

    Template cleanup

--- src/main/kotlin/com/github/lectricas/ideaunknownlang/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/lectricas/ideaunknownlang/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.lectricas.ideaunknownlang.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.lectricas.ideaunknownlang.MyBundle
 
 class MyApplicationService {
 
